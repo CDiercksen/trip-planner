c35f5a417e9f1ac5a4519c2f30fc1d10075d96b0
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-06-26 15:15:17 -0400

    seeded initial destination data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Destination.destroy_all
+
+Destination.create(
+    name: "New York City",
+    image: "http://www.pngmart.com/files/16/New-York-Cityscape-PNG-HD.png"
+)
+Destination.create(
+    name: "Chicago",
+    image: "http://www.pngmart.com/files/6/Chicago-Transparent-PNG.png"
+)
+Destination.create(
+    name: "Moscow",
+    image: "http://www.pngmart.com/files/5/Kremlin-PNG-Clipart.png"
+)
+
+
+puts "seeding reset complete"
